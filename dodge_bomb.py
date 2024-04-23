ee95bbf0c95fa0af5e6f60da0c27dc9135c15280
yamada ayaka <c0a23162fe@edu.teu.ac.jp>
2024-04-23 15:15:42 +0900

    練習問題3

--- dodge_bomb.py
+++ dodge_bomb.py
@@ -7,16 +7,29 @@ import random
 WIDTH, HEIGHT = 1600, 900
 os.chdir(os.path.dirname(os.path.abspath(__file__)))
 
+def check_bound(obj_rct:pg.Rect) -> tuple[bool, bool]:
+    """
+    こうかとんRect, または、爆弾Rectの画面内外判定用の関数
+    引数：こうかとんRect、または、爆弾Rect
+    戻り値：横方向判定結果、縦方向判定結果（True：画面内/False：画面外）
+    """
+    yoko, tate = True, True
+    if obj_rct.left < 0 or WIDTH < obj_rct.right:
+        yoko = False
+    if obj_rct.top < 0 or HEIGHT < obj_rct.bottom:
+        tate = False
+    return yoko, tate
+
 
 def main():
     pg.display.set_caption("逃げろ！こうかとん")
-    screen = pg.display.set_mode((WIDTH, HEIGHT))
+    screen = pg.display.set_mode((WIDTH, HEIGHT))  #背景
     bg_img = pg.image.load("fig/pg_bg.jpg")    
-    kk_img = pg.transform.rotozoom(pg.image.load("fig/3.png"), 0, 2.0)
+    kk_img = pg.transform.rotozoom(pg.image.load("fig/3.png"), 0, 2.0) #こうかとん
     kk_rct = kk_img.get_rect()
     kk_rct.center = 900, 400
     clock = pg.time.Clock()
-    bakudan = pg.Surface((20, 20))
+    bakudan = pg.Surface((20, 20)) #爆弾
     pg.draw.circle(bakudan, (255, 0, 0), (10, 10), 10)
     bakudan.set_colorkey((0, 0, 0))
     bd_rct = bakudan.get_rect()
@@ -38,11 +51,22 @@ def main():
             if key_lst[k]:
                 sum_mv[0] += v[0]
                 sum_mv[1] += v[1]
+
+        if bd_rct[0] >= 100:
+            print("a")
     
         kk_rct.move_ip(sum_mv)
+        if check_bound(kk_rct) != (True, True):
+            kk_rct.move_ip(-sum_mv[0], -sum_mv[1])
         screen.blit(kk_img, kk_rct)
         bd_rct.move_ip(vx, vy)
         screen.blit(bakudan, bd_rct)
+        yoko, tate = check_bound(bd_rct)
+        if not yoko:
+            vx *= -1
+        if not tate:
+            vy *= -1
+
         pg.display.update()
         tmr += 1
         clock.tick(50)
