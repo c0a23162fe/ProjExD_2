07e739ab8ac86771c3acb007df24c49217d5f13b
yamada ayaka <c0a23162fe@edu.teu.ac.jp>
2024-04-23 17:19:48 +0900

    演習問題2

--- dodge_bomb.py
+++ dodge_bomb.py
@@ -9,6 +9,8 @@ import time
 WIDTH, HEIGHT = 1600, 900
 os.chdir(os.path.dirname(os.path.abspath(__file__)))
 
+def 
+
 def check_bound(obj_rct:pg.Rect) -> tuple[bool, bool]:
     """
     こうかとんRect, または、爆弾Rectの画面内外判定用の関数
@@ -22,6 +24,20 @@ def check_bound(obj_rct:pg.Rect) -> tuple[bool, bool]:
         tate = False
     return yoko, tate
 
+def time_acc(tmr, vx, vy) -> tuple[bool, bool, tuple]:
+    bb_imgs=[]
+    accs = [a for a in range(1, 11)]
+    for r in range(1, 11):
+        bb_img = pg.Surface((20*r, 20*r))
+        pg.draw.circle(bb_img, (255, 0, 0), (10*r, 10*r), 10*r)
+        bb_imgs.append(bb_img)
+        avx = vx*accs[min(tmr//500, 9)]
+        avy = vy*accs[min(tmr//500, 9)]
+        bb_img = bb_imgs[min(tmr//500, 9)]
+        return avx,avy, bb_img
+
+
+
 def gameover():
         img = pg.image.load("fig/8.png")
         screen = pg.display.set_mode((WIDTH, HEIGHT))
@@ -29,14 +45,15 @@ def gameover():
         pg.Surface.set_alpha(screen, 50)
         fonto = pg.font.Font(None, 80)
         txt = fonto.render("Game Over", True, (255, 255, 255))
-        screen.blit(img, [600, 450])
-        screen.blit(img, [1000, 450])
-        screen.blit(txt, [675, 450])
+        screen.blit(img, [600, 450])#こうかとん１
+        screen.blit(img, [1000, 450])#こうかとん２
+        screen.blit(txt, [675, 450])#”GAMEOVER”
         pg.display.update()
         time.sleep(5)
 
 
 def main():
+
     pg.display.set_caption("逃げろ！こうかとん")
     screen = pg.display.set_mode((WIDTH, HEIGHT))  #背景
     bg_img = pg.image.load("fig/pg_bg.jpg")    
@@ -61,9 +78,12 @@ def main():
             clock.tick(1)
             gameover()
             return
-            
+        avx, avy, bb_img = time_acc(tmr, vx, vy)
+        print("あ")
+        print(bb_img)
+        print("あ")
         screen.blit(bg_img, [0, 0]) 
-        bd_rct.move_ip(vx, vy)
+        bd_rct.move_ip(avx, avy)
         screen.blit(bakudan, bd_rct)
         key_lst = pg.key.get_pressed()
         sum_mv = [0, 0]
@@ -79,8 +99,10 @@ def main():
         if check_bound(kk_rct) != (True, True):
             kk_rct.move_ip(-sum_mv[0], -sum_mv[1])
         screen.blit(kk_img, kk_rct)
-        bd_rct.move_ip(vx, vy)
-        screen.blit(bakudan, bd_rct)
+        time_acc(tmr, vx, vy)
+        print(bb_img)
+        bakudan1 = pg.transform.scale(bakudan, (20,20))
+        screen.blit(bakudan1, bd_rct)
         yoko, tate = check_bound(bd_rct)
         if not yoko:
             vx *= -1
